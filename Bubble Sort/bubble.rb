746abe376111bb6cec471f99306a76dfab7dd67a
Endand <endand2898@gmail.com>
2024-03-08 23:17:17 -0500

    Finished bubble sort project

---
+++ Bubble Sort/bubble.rb
@@ -0,0 +1,21 @@
+def bubble_sort(arr)
+   (arr.length-1).downto(1) do |i|
+      ptr1=0
+      ptr2=1
+      while ptr2<=i 
+         
+         if arr[ptr1]>arr[ptr2]
+            temp=arr[ptr1]
+            arr[ptr1]=arr[ptr2]
+            arr[ptr2]=temp
+            
+         end
+         ptr1+=1
+         ptr2+=1
+      end
+      
+   end
+   arr
+end
+
+p bubble_sort([4,3,78,2,0,2])
